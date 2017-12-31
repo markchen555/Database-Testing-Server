8800322469eda12a979c00b363c1e79d5642722b
Mark Chen <markchen555@gmail.com>
2017-12-30 17:34:59 -0800

    Doc: modify express server and add eslint, webpack and eiditorconfig

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// disable eslint
+/* eslint-disable no-console*/
+
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
@@ -14,15 +17,14 @@ const PORT = process.env.PORT || 3005;
 const app = express();
 
 app.listen(PORT, err => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(`
+  if (err) {
+    throw err;
+  } else {
+    console.log(`
         It's listening to port: ${PORT}
         ---
         Running on ${process.env.NODE_ENV}
         ---
-      `)
-    }
+    `);
   }
-);
+});
