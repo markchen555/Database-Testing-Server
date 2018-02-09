bfa492fa87dd10871b31b2b32dbd642879d3f169
Mark Chen <markchen555@gmail.com>
2018-02-08 16:39:01 -0800

    Implemented login route

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 // const { Router } = require('express');
+import validate from 'express-validation';
+
+import { authLocal } from '../../server/services/auth';
 
 import * as userController from '../controller/userController';
 // const { * } = require('../controller/userController');
+import userValidation from '../../db/validation/user_validations';
 
 const routes = new Router();
 
-routes.post('/signup', userController.signUp);
+routes.post('/signup', validate(userValidation.signup), userController.signUp);
+routes.post('/login', authLocal, userController.login);
 
 export default routes;
