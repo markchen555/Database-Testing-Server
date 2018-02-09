23eebd86f03a1c8b554269115c7972e23be92585
Mark Chen <markchen555@gmail.com>
2018-02-08 16:35:14 -0800

    Doc: intergrated bcrypt and implemented haspassword and authenticateUser methods

--- db/model/userModel.js
+++ db/model/userModel.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import validator from 'validator';
+import { hashSync, compareSync } from 'bcrypt-nodejs';
 
 // Use destruction if the import file is not export default. In this case it's export const.
 import { passwordReg } from '../validation/user_validations';
@@ -21,24 +22,53 @@ const UserSchema = new Schema({
     type: String,
     required: [true, 'FirstName is required!'],
     trim: true,
+    unique: true,
   },
   lastName: {
     type: String,
     required: [true, 'LastName is required!'],
     trim: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: [true, 'UserName is required!'],
+    trim: true,
+    unique: true,
   },
   password: {
     type: String,
     required: [true, 'Password is required!'],
     trim: true,
-    minlength: [6, 'Password ned to be longer then 6 characters'],
+    minlength: [6, 'Password need to be longer!'],
     validate: {
       validator(password) {
         return passwordReg.test(password);
       },
-      message: '{VALUE} is not a valid password',
+      message: '{VALUE} is not a valid password!',
     },
   },
 });
+
+// MongoDB build in feature
+// This feature will triger if we update a user
+UserSchema.pre('save', function (next) {
+  // This in here is the user
+  // If we don't use isModifed, the password will bcrypt again.
+  if (this.isModified('password')) {
+    // Method is availabe in "this" to use
+    this.password = this._hashPassword(this.password);
+  }
+  return next();
+});
+
+UserSchema.methods = {
+  _hashPassword(password) {
+    return hashSync(password);
+  },
+  _authenticateUser(password) {
+    return compareSync(password, this.password);
+  },
+};
 
 export default mongoose.model('User', UserSchema);
